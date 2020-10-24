a651ef03f01ea55a8d700050a08a0de20fe27d8d
David <dhahn123@gmail.com>
2020-10-23 19:26:15 -0700

    set up model: chess.js, chesspiece.js, square.js

---
+++ src/chess/model/chess.js
@@ -0,0 +1,20 @@
+class Game {
+    constructor() {
+        this.thisPlayerIsPlayingAsWhite = thisPlayerIsPlayingAsWhite // boolean
+        this.chessBoard = this.makeStartingBoard()
+    }
+
+    makeStartingBoard() {
+        // make the board orientation for the current player
+        if (this.thisPlayerIsPlayingAsWhite) {
+            // make chess orientation for white
+        } else {
+            // make chess orientation tailored for black
+        }
+    }
+
+    movePiece(pieceId, to) {
+        // pieceId: string
+        // to: [x, y]
+    }
+}
