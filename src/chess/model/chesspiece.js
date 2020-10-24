14794a6ee9913e8df02b0bd92259f74302ce6a03
David <dhahn123@gmail.com>
2020-10-23 22:45:18 -0700

    finish chesspiece.js

--- src/chess/model/chesspiece.js
+++ src/chess/model/chesspiece.js
@@ -7,12 +7,33 @@ class ChessPiece {
     }
 
     setSquare() {
-        // assign this piece to a specific square
-        // this.square = square
+        // set the square this piece is sitting top of.
+        // on any given piece (on the board), there will always be a piece on top of it.
+        // console.log(newSquare)
+        if (newSquare === undefined) {
+            this.squareThisPieceIsOn = newSquare
+            return
+        }
+
+        if (this.squareThisPieceIsOn === undefined) {
+            this.squareThisPieceIsOn = newSquare
+            newSquare.setPiece(this)
+        }
+
+        const isNewSquareDifferent = this.squareThisPieceIsOn.x != newSquare.x || this.squareThisIsPieceIsOn.y != newSquare.y
+
+        if (isNewSquareDifferent) {
+            // console.log("set")
+            this.squareThisPieceIsOn = newSquare
+            newSquare.setPiece(this)
+        }
     }
 
     getSquare() {
         // get the current square this piece is on.
         // undefined if this piece is not on the board.
+        return this.squareThisPieceIsOn
     }
 }
+
+export default ChessPiece
